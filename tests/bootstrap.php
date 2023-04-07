b3996e9a11889ee7543cb5c24cadc7e26fbb63b2
Borna Butković <dev@bbutkovic.com>
2023-04-07 09:39:45 +0200

    main module name

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -29,7 +29,7 @@ require_once "{$_tests_dir}/includes/functions.php";
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/fastly.php';
+	require dirname(__FILE__, 2) . '/purgely.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
